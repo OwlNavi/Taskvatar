3d0f6d508774feedf0a0126214bf602f7fac5aa8
kurdtpage <kurdtpage@gmail.com>
2018-05-09 13:46:36 +1200

    Can click on pepe and he is bigger

--- app/src/main/java/com/example/cwagt/taskapp345/AvatarHome.java
+++ app/src/main/java/com/example/cwagt/taskapp345/AvatarHome.java
@@ -1,7 +1,17 @@
 package com.example.cwagt.taskapp345;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class AvatarHome extends AppCompatActivity {
+	ImageView avatar;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.avatar_home);
+
+		avatar = findViewById(R.id.avatar);
+	}
 }

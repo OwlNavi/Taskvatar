25f71cea60fef37c1f426485d1113c466ac71e25
miho19 <miho19@hotmail.co.nz>
2018-05-09 15:38:39 +1200

    Added toolbar to top

--- app/src/main/java/com/example/cwagt/taskapp345/MainActivity.java
+++ app/src/main/java/com/example/cwagt/taskapp345/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.cwagt.taskapp345;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         avatar = findViewById(R.id.avatar);
         avatar.setOnClickListener(new View.OnClickListener() {

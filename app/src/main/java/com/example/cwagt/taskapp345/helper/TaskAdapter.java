67fbd12e767d980bffc713849d572905633857d3
Craig <cwagthomas@gmail.com>
2018-05-23 14:14:47 +1200

    Implemented task colors, toggleable colours

--- app/src/main/java/com/example/cwagt/taskapp345/helper/TaskAdapter.java
+++ app/src/main/java/com/example/cwagt/taskapp345/helper/TaskAdapter.java
@@ -1,7 +1,11 @@
 package com.example.cwagt.taskapp345.helper;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +17,7 @@ import android.widget.TextView;
 import com.example.cwagt.taskapp345.R;
 import com.example.cwagt.taskapp345.object.Task;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> {
@@ -39,14 +44,43 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> 
 		final View itemView = LayoutInflater.from(parent.getContext())
 				.inflate(R.layout.movie_list_row, parent, false);
 
+		itemView.setBackgroundColor(Color.RED);
+
 		final RecyclerView recyclerView = parent.findViewById(R.id.taskList);
 		recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(recyclerView.getContext(), recyclerView, new RecyclerItemClickListener.ClickListener() {
 			@Override
 			public void onClick(View view, int position) {
-				Log.d("debug", "click");
+				//Log.d("debug", "click");
 
-                // taskView = recyclerView.getChildAt(position);
-                view.setBackgroundColor(Color.parseColor("#e7eecc"));
+                //change background colour
+                //see https://stackoverflow.com/questions/36352945/how-to-get-color-of-a-button-with-ripple-drawable
+                Drawable background = view.getBackground();
+
+                int backgroundColor = 0;
+
+                if (background instanceof ColorDrawable)
+                    backgroundColor = ((ColorDrawable) background).getColor();
+
+                //Log.d("debug", "Background color: " + backgroundColor);
+
+                //change color
+                switch (backgroundColor){
+                    case Color.WHITE:
+                        view.setBackgroundColor(Color.RED);
+                        break;
+                    case Color.RED:
+                        view.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case Color.YELLOW:
+                        view.setBackgroundColor(Color.GREEN);
+                        break;
+                    case Color.GREEN:
+                        view.setBackgroundColor(Color.RED);
+                        break;
+                    default:
+                        view.setBackgroundColor(Color.WHITE);
+                        break;
+                }
 
 			}
 
@@ -54,8 +88,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> 
 			public void onLongClick(View view, int position) {
 				Log.d("debug", "long click");
 
-                //View taskView = recyclerView.getChildAt(position);
-                view.setBackgroundColor(Color.parseColor("#aaaaaa"));
+                view.setBackgroundColor(Color.RED);
 			}
 		}));
 

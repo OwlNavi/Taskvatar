8a6ce7fc5d0c547697dc1a25a797597f6d66ee43
kurdtpage <kurdtpage@gmail.com>
2018-07-13 10:05:37 +1200

    Created user class

---
+++ app/src/main/java/com/example/cwagt/taskapp345/object/User.java
@@ -0,0 +1,26 @@
+package com.example.cwagt.taskapp345.object;
+
+public class User {
+
+	private String userName;
+
+	public User(String userName){
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"userName='" + userName + '\'' +
+				'}';
+	}
+
+}

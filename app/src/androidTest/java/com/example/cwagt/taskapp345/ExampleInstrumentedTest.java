1b386af7a81b3a4763d5ebad3fe693416f20fc13
kurdtpage <kurdtpage@gmail.com>
2018-07-27 12:24:24 +1200

    Tidied up and finished testing of Tasks, Users and Avatars

--- app/src/androidTest/java/com/example/cwagt/taskapp345/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cwagt/taskapp345/ExampleInstrumentedTest.java
@@ -14,12 +14,18 @@ import org.junit.runner.RunWith;
 import java.util.ArrayList;
 import java.util.Random;
 
+import static com.example.cwagt.taskapp345.helper.DatabaseColumnNames.Task.TASK_NAME_DESCRIPTION;
 import static com.example.cwagt.taskapp345.helper.DatabaseColumnNames.Task.TASK_NAME_TEXT;
+import static com.example.cwagt.taskapp345.helper.DatabaseColumnNames.Task.TASK_NAME_TIME;
+import static com.example.cwagt.taskapp345.helper.DatabaseColumnNames.User.USER_NAME_DESCRIPTION;
+import static com.example.cwagt.taskapp345.helper.DatabaseColumnNames.User.USER_NAME_ID;
+import static com.example.cwagt.taskapp345.helper.DatabaseColumnNames.User.USER_NAME_NAME;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.deleteAvatarFromDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.deleteTaskFromDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.deleteUserFromDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.getAvatarsFromDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.getTasksFromDatabase;
+import static com.example.cwagt.taskapp345.helper.DatabaseHelper.getUsersFromDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.writeAvatarToDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.writeTaskToDatabase;
 import static com.example.cwagt.taskapp345.helper.DatabaseHelper.writeUserToDatabase;
@@ -41,30 +47,19 @@ public class ExampleInstrumentedTest {
     }
 
 
-    private Task createNewTask(){
+    private Task createRandomTask(){
 		Random rand = new Random();
 		int n = rand.nextInt(1000) + 1;
 
-		String name = "Dummy" + n;
+		String name = "Dummy task " + n;
 		String descr = "Dummy descr";
 		String time = "12:00";
 
-		//create task
-		Task task = new Task(name, descr, time);
-		return task;
+		return new Task(name, descr, time);
 	}
 
-	private Task createNewTask(String name, String desc, String time){
-		Random rand = new Random();
-		int n = rand.nextInt(1000) + 1;
-
-		String name = name + n;
-		String descr = desc;
-		String time = time;
-
-		//create task
-		Task task = new Task(name, descr, time);
-		return task;
+	private Task createTask(String name, String desc, String time){
+		return new Task(name, desc, time);
 	}
 
 	@Test
@@ -72,7 +67,7 @@ public class ExampleInstrumentedTest {
 		Context context = InstrumentationRegistry.getTargetContext();
 
 		//create task
-		Task task = createNewTask();
+		Task task = createRandomTask();
 		long rowID = writeTaskToDatabase(context, task);
 		assertNotEquals(-1, rowID);
 
@@ -84,13 +79,20 @@ public class ExampleInstrumentedTest {
 	@Test
 	public void readTaskFromDB(){
 		Context context = InstrumentationRegistry.getTargetContext();
-
-		Task task = createNewTask();
+		
+		Random rand = new Random();
+		int n = rand.nextInt(1000) + 1;
+		
+		String name = "Dummy task " + n;
+		String descr = "Dummy descr";
+		String time = "12:00";
+		
+		Task task = createTask(name, descr, time);
 		long rowID = writeTaskToDatabase(context, task);
 		assertNotEquals(-1, rowID);
 
 		//read task
-		ArrayList<Task> allTasks = getTasksFromDatabase(context, TASK_NAME_TEXT + " = ?", new String[]{name});
+		ArrayList<Task> allTasks = getTasksFromDatabase(context, TASK_NAME_TEXT + " = ? AND " + TASK_NAME_DESCRIPTION + " = ? AND " + TASK_NAME_TIME + " = ?", new String[]{name, descr, time});
 		assertEquals(1, allTasks.size());
 		Task taskFromDb = allTasks.get(0);
 		//assertArrayEquals(task, taskFromDb);
@@ -103,20 +105,22 @@ public class ExampleInstrumentedTest {
 		assertNotEquals(-1, isDeleted);
 	}
 
+	private User createRandomUser(){
+		Random rand = new Random();
+		int n = rand.nextInt(1000) + 1;
+		
+		String userName = "Dummy user " + n;
+		String userDescription = "Testing only";
+		
+		return new User(userName, n, userDescription);
+	}
 
 	@Test
 	public void writeUserToDB(){
 		Context context = InstrumentationRegistry.getTargetContext();
 
-		Random rand = new Random();
-		int n = rand.nextInt(1000) + 1;
-
-		String userName = "Shaun" + n;
-		int userID = n;
-		String userDescription = "Testing only";
-
 		//create user
-		User user = new User(userName, userID, userDescription);
+		User user = createRandomUser();
 		long rowID = writeUserToDatabase(context, user);
 		assertNotEquals(-1, rowID);
 
@@ -124,58 +128,59 @@ public class ExampleInstrumentedTest {
 		int isDeleted = deleteUserFromDatabase(context, user);
 		assertNotEquals(-1, isDeleted);
 	}
-
+	
+	private User createUser(String userName, int userID, String descr){
+		return new User(userName, userID, descr);
+	}
+	
 	@Test
 	public void readUserFromDB(){
 		Context context = InstrumentationRegistry.getTargetContext();
 
 		Random rand = new Random();
-		int n = rand.nextInt(1000) + 1;
-
-		String name = "Dummy" + n;
+		int userID = rand.nextInt(1000) + 1;
+		String userName = "Dummy user " + userID;
 		String descr = "Dummy descr";
-		String time = "12:00";
 
-		//create task
-		Task task = new Task(name, descr, time);
-		long rowID = writeTaskToDatabase(context, task);
+		//create user
+		User user = createUser(userName, userID, descr);
+		long rowID = writeUserToDatabase(context, user);
 		assertNotEquals(-1, rowID);
 
-		//read task
-		ArrayList<Task> allTasks = getTasksFromDatabase(context, TASK_NAME_TEXT + " = ?", new String[]{name});
-		assertEquals(1, allTasks.size());
-		Task taskFromDb = allTasks.get(0);
+		//read user
+		ArrayList<User> allUsers = getUsersFromDatabase(context, USER_NAME_NAME + " = ? AND " + USER_NAME_ID + " = ? AND " + USER_NAME_DESCRIPTION + " = ?", new String[]{userName, String.valueOf(userID), descr});
+		assertEquals(1, allUsers.size());
+		User userFromDb = allUsers.get(0);
 		//assertArrayEquals(task, taskFromDb);
-		assertEquals(name, taskFromDb.getName());
-		assertEquals(descr, taskFromDb.getDescription());
-		assertEquals(time, taskFromDb.getTime());
+		assertEquals(userName, userFromDb.getUserName());
+		assertEquals(userID, userFromDb.getUserID());
+		assertEquals(descr, userFromDb.getUserDescription());
 
 		//delete task
-		int isDeleted = deleteTaskFromDatabase(context, task);
+		int isDeleted = deleteUserFromDatabase(context, user);
 		assertNotEquals(-1, isDeleted);
 	}
 
 
-	@Test
-	public void writeAvatarToDB(){
-		Context context = InstrumentationRegistry.getTargetContext();
-
+	private Avatar createRandomAvatar(User user){
 		Random rand = new Random();
-		int n = rand.nextInt(1000) + 1;
-
+		
 		float leftArm = rand.nextInt(360) + 1;
 		float rightArm = rand.nextInt(360) + 1;
 		float leftLeg = rand.nextInt(360) + 1;
 		float rightLeg = rand.nextInt(360) + 1;
+		
+		return new Avatar(leftArm, rightArm, leftLeg, rightLeg, user);
+	}
+	
+	@Test
+	public void writeAvatarToDB(){
+		Context context = InstrumentationRegistry.getTargetContext();
 
-		String userName = "Shaun" + n;
-		String userDescription = "Testing only";
-
-		//create user
-		User user = new User(userName, n, userDescription);
+		User user = createRandomUser();
 
 		//create avatar
-		Avatar avatar = new Avatar(leftArm, rightArm, leftLeg, rightLeg, user);
+		Avatar avatar = createRandomAvatar(user);
 		long rowID = writeAvatarToDatabase(context, avatar);
 		assertNotEquals(-1, rowID);
 
@@ -188,25 +193,25 @@ public class ExampleInstrumentedTest {
 		assertNotEquals(-1, isDeleted);
 	}
 
+	private Avatar createAvatar(float leftArm, float rightArm, float leftLeg, float rightLeg, User user){
+    	return new Avatar(leftArm, rightArm, leftLeg, rightLeg, user);
+	}
+	
 	@Test
 	public void readAvatarFromDB(){
 		Context context = InstrumentationRegistry.getTargetContext();
 
 		Random rand = new Random();
-		int n = rand.nextInt(1000) + 1;
 		float leftArm = rand.nextInt(360) + 1;
 		float rightArm = rand.nextInt(360) + 1;
 		float leftLeg = rand.nextInt(360) + 1;
 		float rightLeg = rand.nextInt(360) + 1;
 
-		String userName = "Dummy" + n;
-		String userDesc = "Dummy descr";
-
 		//create user
-		User user = new User(userName, n, userDesc);
+		User user = createRandomUser();
 
 		//create avatar
-		Avatar avatar = new Avatar(leftArm, rightArm, leftLeg, rightLeg, user);
+		Avatar avatar = createAvatar(leftArm, rightArm, leftLeg, rightLeg, user);
 		long rowID = writeAvatarToDatabase(context, avatar);
 		assertNotEquals(-1, rowID);
 
@@ -217,6 +222,8 @@ public class ExampleInstrumentedTest {
 		//assertArrayEquals(avatar, avatarFromDb);
 		assertEquals(leftArm, avatarFromDb.getLeftArmRotation(), 0.1);
 		assertEquals(rightArm, avatarFromDb.getRightArmRotation(), 0.1);
+		assertEquals(leftLeg, avatarFromDb.getLeftLegRotation(), 0.1);
+		assertEquals(rightLeg, avatarFromDb.getRightLegRotation(), 0.1);
 
 		//delete user
 		int delUser = deleteUserFromDatabase(context, user);

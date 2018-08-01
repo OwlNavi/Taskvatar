91aa2157cf743a484970961f63071c7a0917f130
Craig <cwagthomas@gmail.com>
2018-08-01 14:11:07 +1200

    AddUser function now adds to database

--- app/src/main/java/com/example/cwagt/taskapp345/view/AddUser.java
+++ app/src/main/java/com/example/cwagt/taskapp345/view/AddUser.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.cwagt.taskapp345.R;
+import com.example.cwagt.taskapp345.helper.DatabaseHelper;
+import com.example.cwagt.taskapp345.object.User;
+
+import java.util.ArrayList;
 
 
 /**
@@ -29,6 +34,22 @@ public class AddUser extends AppCompatActivity {
         final Button button = findViewById(R.id.submitButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //Create new user
+                TextView usernameField = findViewById(R.id.edittextUserName);
+                TextView lastNameField = findViewById(R.id.edittextUserLastName);
+                TextView firstNameField = findViewById(R.id.edittextUserFirstName);
+                String username = usernameField.getText().toString();
+                String firstName = firstNameField.getText().toString();
+                String lastName = lastNameField.getText().toString();
+                String fullName = firstName + " " + lastName;
+
+                ArrayList<User> users = DatabaseHelper.getAllUsersFromDatabase(context);
+
+                int userID = users.size()+1;
+                User newUser = new User(username, userID, fullName);
+
+                DatabaseHelper.writeUserToDatabase(context, newUser);
+
                 // Code here executes on main thread after user presses button
                 Intent addUserIntent = new Intent(context, UserHome.class);
                 startActivity(addUserIntent);

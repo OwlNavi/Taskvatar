329f0addeb73b64049e716d34f2b68b1954dd301
shenderson <(h0m3)g1thub>
2018-08-03 10:28:41 +1200

    Changed avatar constructor parameters for testing

--- app/src/test/java/com/example/cwagt/taskapp345/ExampleUnitTest.java
+++ app/src/test/java/com/example/cwagt/taskapp345/ExampleUnitTest.java
@@ -2,7 +2,10 @@ package com.example.cwagt.taskapp345;
 
 import com.example.cwagt.taskapp345.object.Avatar;
 
+import com.example.cwagt.taskapp345.object.User;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -19,7 +22,7 @@ public class ExampleUnitTest {
 
     @Test
 	public void avatarGetRotationDefault() {
-    	//tests the getter
+    	//tests the getter, default constructor
     	Avatar testAvatar1 = new Avatar();
     	assertEquals(Float.valueOf(0), (Float)testAvatar1.getLeftArmRotation());
 		assertEquals(Float.valueOf(0), (Float)testAvatar1.getRightArmRotation());
@@ -29,12 +32,24 @@ public class ExampleUnitTest {
 
 	@Test
 	public void avatarGetRotationExplicit() {
-		//tests the getter
-		Avatar testAvatar2 = new Avatar(1,2,3,4, null);
-		assertEquals(Float.valueOf(1), (Float)testAvatar2.getLeftArmRotation());
-		assertEquals(Float.valueOf(2), (Float)testAvatar2.getRightArmRotation());
-		assertEquals(Float.valueOf(3), (Float)testAvatar2.getLeftLegRotation());
-		assertEquals(Float.valueOf(4), (Float)testAvatar2.getRightLegRotation());
+		//tests the getter, explicit constructor
+		Random rand = new Random();
+
+		Float degrees = (float)360;
+
+		Integer avatarID = rand.nextInt(100) + 1;
+		Float leftArmRotation = rand.nextFloat() * degrees;
+		Float rightArmRotation = rand.nextFloat() * degrees;
+		Float leftLegRotation = rand.nextFloat() * degrees;
+		Float rightLegRotation = rand.nextFloat() * degrees;
+
+		User user = new User("Dummy user " + avatarID, avatarID, "Desc");
+
+		Avatar testAvatar2 = new Avatar(avatarID, "base_red", "left_arm", "right_arm", "left_leg", "right_leg", user, leftArmRotation, rightArmRotation, leftLegRotation, rightLegRotation);
+		assertEquals(leftArmRotation, testAvatar2.getLeftArmRotation(), 0.1);
+		assertEquals(rightArmRotation, testAvatar2.getRightArmRotation(), 0.1);
+		assertEquals(leftLegRotation, testAvatar2.getLeftLegRotation(), 0.1);
+		assertEquals(rightLegRotation, testAvatar2.getRightLegRotation(), 0.1);
 	}
 
 	@Test
@@ -48,8 +63,28 @@ public class ExampleUnitTest {
 	@Test
 	public void avatarSetLeftArmRotationExplicit() {
 		//tests the setter (and the getter)
-		Avatar testAvatar4 = new Avatar(1,2,3,4,null);
-		testAvatar4.setLeftArmRotation(5);
-		assertEquals(Float.valueOf(5), (Float)testAvatar4.getLeftArmRotation());
+		Random rand = new Random();
+
+		Float degrees = (float)360;
+
+		Integer avatarID = rand.nextInt(100) + 1;
+		Float leftArmRotation = rand.nextFloat() * degrees + 10;
+		Float rightArmRotation = rand.nextFloat() * degrees + 10;
+		Float leftLegRotation = rand.nextFloat() * degrees + 10;
+		Float rightLegRotation = rand.nextFloat() * degrees + 10;
+
+		User user = new User("Dummy user " + avatarID, avatarID, "Desc");
+
+		Avatar testAvatar4 = new Avatar(avatarID, "base_red", "left_arm", "right_arm", "left_leg", "right_leg", user, leftArmRotation, rightArmRotation, leftLegRotation, rightLegRotation);
+
+		testAvatar4.setLeftArmRotation(2);
+		testAvatar4.setRightArmRotation(3);
+		testAvatar4.setLeftLegRotation(4);
+		testAvatar4.setRightLegRotation(5);
+
+		assertEquals(Float.valueOf(2), (Float)testAvatar4.getLeftArmRotation());
+		assertEquals(Float.valueOf(3), (Float)testAvatar4.getRightArmRotation());
+		assertEquals(Float.valueOf(4), (Float)testAvatar4.getLeftLegRotation());
+		assertEquals(Float.valueOf(5), (Float)testAvatar4.getRightLegRotation());
 	}
 }

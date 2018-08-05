ccd9554911fed45539719a514ae7c06d25df6033
Craig <cwagthomas@gmail.com>
2018-08-05 14:37:32 +1200

    Added documentation and removed unused methods in User class

--- app/src/main/java/com/example/cwagt/taskapp345/object/User.java
+++ app/src/main/java/com/example/cwagt/taskapp345/object/User.java
@@ -1,52 +1,45 @@
 package com.example.cwagt.taskapp345.object;
 
+/**
+ * This is the class representation of Users
+ */
 public class User {
-
+	//The chosen username for the user
 	private String userName;
+	//The unique identifier for the user
 	private int userID;
+	//A description for the user, currently their full name
 	private String userDescription;
+	//Each user has their own avatar
 	private Avatar avatar;
 
+	/**
+	 * Constructor for Users
+	 * @param userName the username of the User
+	 * @param userID the User's unique identifier
+	 * @param userDescription the user's fullname
+	 */
 	public User(String userName, int userID, String userDescription) {
-		//get from database
 		this.userName = userName;
 		this.userID = userID;
 		this.userDescription = userDescription;
 
-		//make avatar from db info
+		//make avatar from db info from the userID
 		//avatar = new Avatar();
 	}
 
-	public int getUserID() {
-
-		return userID;
-	}
-
-	public void setUserID(int userID) {
-		this.userID = userID;
-	}
+	/**
+	 * Returns the users unique identifier ID
+	 * @return the user's ID
+	 */
+	public int getUserID() { return userID;	}
 
 	public String getUserDescription() {
 		return userDescription;
 	}
 
-	public void setUserDescription(String userDescription) {
-		this.userDescription = userDescription;
-	}
-
 	public String getUserName() {
 		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-				"userName='" + userName + '\'' +
-				'}';
 	}
 
 }

3b74e773ce330fec30df7ba493cafab35426162e
Craig <cwagthomas@gmail.com>
2018-08-05 15:06:00 +1200

    Added documentation for AddUser activity class

--- app/src/main/java/com/example/cwagt/taskapp345/view/AddUser.java
+++ app/src/main/java/com/example/cwagt/taskapp345/view/AddUser.java
@@ -7,34 +7,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.cwagt.taskapp345.R;
 import com.example.cwagt.taskapp345.helper.DatabaseHelper;
 import com.example.cwagt.taskapp345.object.User;
-
 import java.util.ArrayList;
-
 
 /**
  * Created by cwagt on 15/07/2018.
+ *
+ * This class allows the user to create a new user account
  */
 
 public class AddUser extends AppCompatActivity {
-
+    //The context of the current activity
     private Context context;
 
-
+    /**
+     * Code executed when the activity is loaded
+     * @param savedInstanceState the savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Get the current context
         context = getApplicationContext();
+        //Set the activity layout based on xml file
         setContentView(R.layout.add_user);
 
-        //Button code
+        //Button code when a user wants to add a new user
         final Button button = findViewById(R.id.submitButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Create new user
+                //Retrieve the information the user inputted
                 TextView usernameField = findViewById(R.id.edittextUserName);
                 TextView lastNameField = findViewById(R.id.edittextUserLastName);
                 TextView firstNameField = findViewById(R.id.edittextUserFirstName);
@@ -43,18 +47,22 @@ public class AddUser extends AppCompatActivity {
                 String lastName = lastNameField.getText().toString();
                 String fullName = firstName + " " + lastName;
 
+                //Retrieve the list of users from the database so we can check the number of users
                 ArrayList<User> users = DatabaseHelper.getAllUsersFromDatabase(context);
 
+                //The new user needs to have a uniqueID so we set it to the number of users plus
+                //one, this way all the users should have a unique id
                 int userID = users.size()+1;
-                User newUser = new User(username, userID, fullName);
 
+                //Create the new user
+                User newUser = new User(username, userID, fullName);
+                //Save them to the database
                 DatabaseHelper.writeUserToDatabase(context, newUser);
 
-                // Code here executes on main thread after user presses button
+                //Go back to the choose user page now the user has been saved
                 Intent addUserIntent = new Intent(context, UserHome.class);
                 startActivity(addUserIntent);
             }
         });
     }
-
 }

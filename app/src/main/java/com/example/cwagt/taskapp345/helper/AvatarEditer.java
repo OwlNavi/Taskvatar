3cd3815f7b6b41e33be4da81ffd10193bdf25374
Craig <cwagthomas@gmail.com>
2018-08-09 19:55:03 +1200

    AvatarEditer - Added save/load methods and documentation

--- app/src/main/java/com/example/cwagt/taskapp345/helper/AvatarEditer.java
+++ app/src/main/java/com/example/cwagt/taskapp345/helper/AvatarEditer.java
@@ -2,41 +2,63 @@ package com.example.cwagt.taskapp345.helper;
 
 import android.view.View;
 import android.widget.ImageView;
-
 import com.example.cwagt.taskapp345.R;
+import java.util.HashMap;
 
 /**
  * Created by cwagt on 9/08/2018.
+ * This class is used to handle the interactions with the avatar fragment
+ * It is used to edit body parts and save the avatar to the database
  */
-
 public class AvatarEditer {
-    View avatar;
-    ImageView hat;
-    ImageView leftArm;
-    ImageView rightArm;
-    ImageView leftLeg;
-    ImageView rightLeg;
-    ImageView base;
+    private ImageView hat;
+    private ImageView leftArm;
+    private ImageView rightArm;
+    private ImageView leftLeg;
+    private ImageView rightLeg;
+    private ImageView base;
 
-    final int HEAD = 0;
-    final int LEFT_ARM = 1;
-    final int RIGHT_ARM = 2;
-    final int LEFT_LEG = 3;
-    final int RIGHT_LEG = 4;
-    final int TORSO = 5;
-    final int BACKGROUND = 6;
+    private final int HEAD = 0;
+    private final int LEFT_ARM = 1;
+    private final int RIGHT_ARM = 2;
+    private final int LEFT_LEG = 3;
+    private final int RIGHT_LEG = 4;
+    private final int TORSO = 5;
+    private final int BACKGROUND = 6;
 
+    //a data structure linking the name of the body part with the R.drawable id describing the resource
+    private HashMap<String, Integer> bodyParts;
 
+    /**
+     * Avatar Editer constructer
+     * Given the view of the avatar fragment it will locate the body parts to initialise itself
+     * @param avatarFragment the View of the avatar fragment
+     */
     public AvatarEditer(View avatarFragment){
-        this.avatar = avatarFragment;
+        View avatar = avatarFragment;
         this.base = avatar.findViewById(R.id.base);
         this.leftArm = avatar.findViewById(R.id.left_arm);
         this.hat = avatar.findViewById(R.id.hat);
         this.leftLeg = avatar.findViewById(R.id.left_leg);
         this.rightLeg = avatar.findViewById(R.id.right_leg);
         this.rightArm = avatar.findViewById(R.id.right_arm);
+
+        bodyParts = new HashMap<>();
+        bodyParts.put("base", R.id.base);
+        bodyParts.put("hat", R.id.hat);
+        bodyParts.put("leftArm", R.id.left_arm);
+        bodyParts.put("rightArm", R.id.right_arm);
+        bodyParts.put("leftLeg", R.id.left_leg);
+        bodyParts.put("rightLeg", R.id.right_leg);
+        //bodyParts.put("background", R.id.hat);
+
     }
 
+    /**
+     * Modifies the avatar by changing the body part identified by category and item name
+     * @param currentCategory the body part category
+     * @param itemSelected the style selected
+     */
     public void setImage(int currentCategory, String itemSelected) {
         switch (currentCategory){
             case(HEAD):
@@ -58,58 +80,124 @@ public class AvatarEditer {
                 break;
         }
     }
+
+    /**
+     * Sets the left leg based on the item selected
+     * @param itemSelected the name of the item selected
+     */
     private void setLeftLeg(String itemSelected) {
         if(itemSelected.equals("Strong")){
             leftLeg.setImageResource(R.drawable.left_leg_muscly);
+            bodyParts.put("leftLeg", R.drawable.left_arm_muscly);
         }
         if(itemSelected.equals("Robot")){
             leftLeg.setImageResource(R.drawable.left_leg_robot);
+            bodyParts.put("leftLeg", R.drawable.left_leg_robot);
         }
         if(itemSelected.equals("Cartoon")){
             leftLeg.setImageResource(R.drawable.left_leg);
+            bodyParts.put("leftLeg", R.drawable.left_leg);
         }
+
+        saveAvatar();
     }
+
+    /**
+     * Sets the right leg based on the item selected
+     * @param itemSelected the name of the item selected
+     */
     private void setRightLeg(String itemSelected) {
         if(itemSelected.equals("Strong")){
             rightLeg.setImageResource(R.drawable.right_leg_muscly);
+            bodyParts.put("rightLeg", R.drawable.right_leg_muscly);
         }
         if(itemSelected.equals("Robot")){
             rightLeg.setImageResource(R.drawable.right_leg_robot);
+            bodyParts.put("rightLeg", R.drawable.right_leg_robot);
         }
         if(itemSelected.equals("Cartoon")){
             rightLeg.setImageResource(R.drawable.right_leg);
+            bodyParts.put("rightLeg", R.drawable.right_leg);
         }
+
+        saveAvatar();
     }
+
+    /**
+     * Sets the right arm based on the item selected
+     * @param itemSelected the item selected's name
+     */
     private void setRightArm(String itemSelected) {
         if(itemSelected.equals("Strong")){
             rightArm.setImageResource(R.drawable.right_arm_muscly);
+            bodyParts.put("rightArm", R.drawable.right_arm_muscly);
         }
         if(itemSelected.equals("Robot")){
             rightArm.setImageResource(R.drawable.right_arm_robot);
+            bodyParts.put("rightArm", R.drawable.right_arm_robot);
         }
         if(itemSelected.equals("Cartoon")){
             rightArm.setImageResource(R.drawable.right_arm);
+            bodyParts.put("rightArm", R.drawable.right_arm);
         }
+
+        saveAvatar();
     }
 
+    /**
+     * Sets the left arm based on the item selected
+     * @param itemSelected the name of the item selected
+     */
     private void setLeftArm(String itemSelected) {
         if(itemSelected.equals("Strong")){
             leftArm.setImageResource(R.drawable.left_arm_muscly);
+            bodyParts.put("leftArm", R.drawable.left_arm_muscly);
         }
         if(itemSelected.equals("Robot")){
             leftArm.setImageResource(R.drawable.left_arm_robot);
+            bodyParts.put("leftArm", R.drawable.left_arm_robot);
         }
         if(itemSelected.equals("Cartoon")){
             leftArm.setImageResource(R.drawable.left_arm);
+            bodyParts.put("leftArm", R.drawable.left_arm);
         }
+
+        saveAvatar();
     }
 
+    /**
+     * Sets the hat based on the given string
+     * @param itemSelected the name of the item selected
+     */
     private void setHat(String itemSelected) {
         if(itemSelected.equals("Crown")){
             hat.setImageResource(R.drawable.hat_crown);
+            bodyParts.put("hat", R.drawable.hat_crown);
         }
         if(itemSelected.equals("Pirate")){
             hat.setImageResource(R.drawable.hat_pirate);
+            bodyParts.put("hat", R.drawable.hat_pirate);
         }
+
+        saveAvatar();
+    }
+
+    /**
+     * Saves the avatar so it can be retrieved
+     */
+    private void saveAvatar(){
+        //SAVE IN DATABASE
+    }
+
+    /**
+     * Sets the avatar in the view based on the provided hashmap values
+     * @param bodyParts
+     */
+    public void setAvatar(HashMap<String, Integer> bodyParts){
+        hat.setImageResource(bodyParts.get("hat"));
+        leftArm.setImageResource(bodyParts.get("leftArm"));
+        rightArm.setImageResource(bodyParts.get("rightArm"));
+        leftLeg.setImageResource(bodyParts.get("leftLeg"));
+        rightLeg.setImageResource(bodyParts.get("rightLeg"));
     }
 }
